2b63493e0a1ada5fd7423990fb2c2e311963d47a
Linus045 <lwillms88@gmail.com>
2025-03-31 21:40:23 +0200

    chore: adds todo

--- src/app.cpp
+++ src/app.cpp
@@ -87,6 +87,8 @@ void Application::run()
 		    .direction = camera.transform.getForward(),
 		};
 
+		// TODO: test out other definition for normals
+		// Visualize planes
 		glm::vec3 v = (glm::abs(ray.direction.y) < 0.99f) ? glm::vec3(0.0f, 1.0f, 0.0f)
 		                                                  : glm::vec3(1.0f, 0.0f, 0.0f);
 		glm::vec3 n1 = normalize(cross(ray.direction, v));
