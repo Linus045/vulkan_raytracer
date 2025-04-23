8a6a05cc97b15a326d3fb4db2fa32bff54b9bd65
Linus045 <linuswillms00@gmail.com>
2025-04-23 14:34:57 +0200

    refactor: removes unused log method

--- include/logger.hpp
+++ include/logger.hpp
@@ -47,33 +47,3 @@ inline void logMat4(const std::string& info, const glm::mat4& mat)
 	std::printf("%.02f, %.02f, %.02f, %.02f\n", mat[2][0], mat[2][1], mat[2][2], mat[2][3]);
 	std::printf("%.02f, %.02f, %.02f, %.02f\n", mat[3][0], mat[3][1], mat[3][2], mat[3][3]);
 }
-
-inline void logBezierTriangle2(const std::string& info, const BezierTriangle2& bezierTriangle2)
-{
-	std::printf("%s 0: (%.2f,%.2f,%.2f) 1: (%.2f,%.2f,%.2f) 2: (%.2f,%.2f,%.2f) 3: "
-	            "(%.2f,%.2f,%.2f) 4: (%.2f,%.2f,%.2f) 5: (%.2f,%.2f,%.2f)\n",
-	            info.c_str(),
-	            bezierTriangle2.controlPoints[0].x,
-	            bezierTriangle2.controlPoints[0].y,
-	            bezierTriangle2.controlPoints[0].z,
-
-	            bezierTriangle2.controlPoints[1].x,
-	            bezierTriangle2.controlPoints[1].y,
-	            bezierTriangle2.controlPoints[1].z,
-
-	            bezierTriangle2.controlPoints[2].x,
-	            bezierTriangle2.controlPoints[2].y,
-	            bezierTriangle2.controlPoints[2].z,
-
-	            bezierTriangle2.controlPoints[3].x,
-	            bezierTriangle2.controlPoints[3].y,
-	            bezierTriangle2.controlPoints[3].z,
-
-	            bezierTriangle2.controlPoints[4].x,
-	            bezierTriangle2.controlPoints[4].y,
-	            bezierTriangle2.controlPoints[4].z,
-
-	            bezierTriangle2.controlPoints[5].x,
-	            bezierTriangle2.controlPoints[5].y,
-	            bezierTriangle2.controlPoints[5].z);
-}
